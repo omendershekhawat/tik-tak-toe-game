e03d820a5a32c1deeb0a85fdd591fae2e8b4f7af
unknown <omendershekhawat1@gmail.com>
2024-06-23 23:23:18 +0530

    tik-tak-toe-game

---
+++ index.js
@@ -0,0 +1,87 @@
+let boxes = document.querySelectorAll(".box")
+let resetBtn = document.querySelector("#reset-btn")
+let newGameBtn = document.querySelector("#new-btn")
+let msgContainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#msg")
+
+let turn0 = true;
+
+//make 2d array
+const winpatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+//for reset game
+
+const resetGame = ()=>{
+      turn0 = true;
+      enableBoxes ();
+      msgContainer.classList.add("hide")
+
+    
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        
+        if (turn0) {
+            box.innerText = "0"
+            turn0 = false
+        } else {
+            box.innerText = "x "
+            turn0 = true
+        }
+        box.disabled = true
+
+        checkWinner();
+    })
+})
+
+
+const disableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = true;
+    }
+};
+
+
+
+//for enable boxes
+
+const enableBoxes=()=>{
+    for (let box of boxes){
+        box.disabled = false;
+        box.innerText="";
+    }
+};
+//for show the winner
+const showWinner = (winner) => {
+    msg.innerText = `congratulation, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+const checkWinner = () => {
+    for (let pattern of winpatterns) {
+    
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+               
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
